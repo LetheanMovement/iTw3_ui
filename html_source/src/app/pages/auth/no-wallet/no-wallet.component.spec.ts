6ad9fa303d394cfa049d72fe701f11146c229779
Snider <snider@lt.hn>
2023-02-22 16:57:19 +0000

    * moves no-auth file selection away from QT * separates business & display logic

---
+++ html_source/src/app/pages/auth/no-wallet/no-wallet.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NoWalletComponent } from './no-wallet.component';
+
+describe('NoWalletComponent', () => {
+  let component: NoWalletComponent;
+  let fixture: ComponentFixture<NoWalletComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NoWalletComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NoWalletComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- html_source/src/app/pages/auth/no-wallet/no-wallet.component.ts
+++ html_source/src/app/pages/auth/no-wallet/no-wallet.component.ts
@@ -5,74 +5,12 @@ import { BackendService } from '@api/services/backend.service';
 import { VariablesService } from '@parts/services/variables.service';
 import { TranslateService } from '@ngx-translate/core';
 import { paths } from '../../paths';
+import {GetUserSelectedFilePath} from "../../../../assets/wailsjs/go/main/App";
 
 @Component({
   selector: 'app-no-wallet',
-  template: `
-    <div
-      class="no-wallet-wrapper"
-      fxFlexFill
-      fxLayout="row"
-      fxLayoutAlign="center center"
-    >
-      <div
-        class="card max-w-42-rem max-h-100 w-100 p-2 border-radius-0_8-rem bg-light-blue scrolled-content"
-      >
-        <div class="logo mb-3" fxLayout="row" fxLayoutAlign="center center">
-          <img style='width: 100%;' alt="lethean-logo" src="assets/icons/blue/lthn-logo.png" />
-        </div>
-
-        <h4 class="mb-2 text-align-center">{{ 'MAIN.TITLE' | translate }}</h4>
-
-        <button
-          [routerLink]="['/' + paths.create]"
-          class="primary big w-100 mb-1"
-          type="button"
-        >
-          {{ 'MAIN.BUTTON_NEW_WALLET' | translate }}
-        </button>
-
-        <button
-          (click)="openWallet()"
-          class="primary big w-100 mb-1"
-          type="button"
-        >
-          {{ 'MAIN.BUTTON_OPEN_WALLET' | translate }}
-        </button>
-
-        <button
-          [routerLink]="['/' + paths.restore]"
-          class="outline big w-100 mb-2"
-          type="button"
-        >
-          {{ 'MAIN.BUTTON_RESTORE_BACKUP' | translate }}
-        </button>
-
-        <p
-          (click)="openInBrowser()"
-          class="text-align-center cursor-pointer"
-          fxLayout="row"
-          fxLayoutAlign="center center"
-        >
-          <i class="icon question-circle mr-1"></i>
-          <span class="color-primary">{{ 'MAIN.HELP' | translate }}</span>
-        </p>
-      </div>
-
-      <app-synchronization-status
-        class="max-w-19-rem"
-      ></app-synchronization-status>
-    </div>
-  `,
-  styles: [
-    `
-      :host {
-        width: 100%;
-        height: 100%;
-        overflow: hidden;
-      }
-    `,
-  ],
+  templateUrl: './no-wallet.component.html',
+  styleUrls: ['./no-wallet.component.scss']
 })
 export class NoWalletComponent {
   paths = paths;
@@ -86,31 +24,25 @@ export class NoWalletComponent {
   ) {}
 
   openWallet(): void {
-    this.backend.openFileDialog(
-      this.translate.instant('MAIN.CHOOSE_PATH'),
-      '*',
-      this.variablesService.settings.default_path,
-      (file_status, file_data) => {
-        if (file_status) {
-          this.variablesService.settings.default_path = file_data.path.substr(
-            0,
-            file_data.path.lastIndexOf('/')
-          );
-          this.ngZone.run(() => {
-            this.router
-              .navigate(['/' + paths.open], {
-                queryParams: { path: file_data.path },
-              })
-              .then();
-          });
-        } else {
-          console.log(file_data['error_code']);
-        }
+    GetUserSelectedFilePath().then((path) => {
+      if (path) {
+        this.variablesService.settings.default_path = path.substr(
+          0,
+          path.lastIndexOf('/')
+        );
+        this.ngZone.run(() => {
+          this.router
+            .navigate(['/' + paths.open], {
+              queryParams: { path: path },
+            })
+            .then();
+        });
       }
-    );
+    });
   }
 
   openInBrowser(): void {
     this.backend.openUrlInBrowser(CREATE_NEW_WALLET_HELP_PAGE);
   }
 }
+
