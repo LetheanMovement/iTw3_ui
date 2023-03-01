9f7b96527aba81ec8ed22c8425e34f15fd5dfe68
Snider <snider@lt.hn>
2023-03-01 14:43:08 +0000

    file handling update + splits html out of typescript.
    Signed-off-by: Snider <snider@lt.hn>

--- src/app/pages/add-wallet/add-wallet.component.ts
+++ src/app/pages/add-wallet/add-wallet.component.ts
@@ -4,75 +4,12 @@ import { VariablesService } from '@parts/services/variables.service';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { CREATE_NEW_WALLET_HELP_PAGE } from '@parts/data/constants';
+import { GetUserSelectedFilePath } from '../../../assets/wailsjs/go/main/App';
+import { paths } from '../paths';
 
 @Component({
   selector: 'app-add-wallet',
-  template: `
-    <div class="page-container">
-      <div class="toolbar mb-2">
-        <div class="left">
-          <button appBackButton class="btn-icon circle big mr-2" type="button">
-            <i class="icon dropdown-arrow-left"></i>
-          </button>
-          <h1>{{ 'BREADCRUMBS.ADD_WALLET' | translate }}</h1>
-        </div>
-        <div class="right"></div>
-      </div>
-
-      <div class="page-content">
-        <div
-          class="scrolled-content"
-          fxFlex="1 1 auto"
-          fxLayout="column"
-          fxLayoutAlign="center center"
-        >
-          <div
-            class="add-wallet w-100"
-            fxLayout="row"
-            fxLayoutAlign="center center"
-          >
-            <div class="wrap-controls text-align-center max-w-38-rem">
-              <h4 class="mb-2">{{ 'MAIN.TITLE' | translate }}</h4>
-
-              <button
-                [routerLink]="['/create']"
-                class="primary big w-100 mb-1"
-                type="button"
-              >
-                {{ 'MAIN.BUTTON_NEW_WALLET' | translate }}
-              </button>
-
-              <button
-                (click)="openWallet()"
-                class="primary big w-100 mb-1"
-                type="button"
-              >
-                {{ 'MAIN.BUTTON_OPEN_WALLET' | translate }}
-              </button>
-
-              <button
-                [routerLink]="['/restore']"
-                class="outline big w-100 mb-2"
-                type="button"
-              >
-                {{ 'MAIN.BUTTON_RESTORE_BACKUP' | translate }}
-              </button>
-
-              <p
-                (click)="openInBrowser()"
-                class="text-align-center cursor-pointer"
-                fxLayout="row"
-                fxLayoutAlign="center center"
-              >
-                <i class="icon question-circle mr-1"></i>
-                <span class="color-primary">{{ 'MAIN.HELP' | translate }}</span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  `,
+  templateUrl: './add-wallet.component.html',
   styles: [
     `
       :host {
@@ -93,26 +30,21 @@ export class AddWalletComponent {
   ) {}
 
   openWallet(): void {
-    this.backendService.openFileDialog(
-      this.translate.instant('MAIN.CHOOSE_PATH'),
-      '*',
-      this.variablesService.settings.default_path,
-      async (file_status, file_data) => {
-        if (file_status) {
-          this.variablesService.settings.default_path = file_data.path.substr(
-            0,
-            file_data.path.lastIndexOf('/')
-          );
-          await this.ngZone.run(async () => {
-            await this.router.navigate(['/open'], {
-              queryParams: { path: file_data.path },
-            });
-          });
-        } else {
-          console.error(file_data['error_code']);
-        }
+    GetUserSelectedFilePath().then((path) => {
+      if (path) {
+        this.variablesService.settings.default_path = path.substr(
+          0,
+          path.lastIndexOf('/')
+        );
+        this.ngZone.run(() => {
+          this.router
+            .navigate(['/' + paths.open], {
+              queryParams: { path: path },
+            })
+            .then();
+        });
       }
-    );
+    });
   }
 
   openInBrowser(): void {
